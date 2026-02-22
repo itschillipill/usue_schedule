e60e968e0fda48eac6a4ceebc4e64afd8714b825
Mehrob02 <mehrobjonabdulvahobov@gmail.com>
2026-02-22 19:37:26 +0500

    Formatted

--- test/utils/date_utils_test.dart
+++ test/utils/date_utils_test.dart
@@ -3,10 +3,11 @@ import 'package:usue_schedule/core/utils/date_utils.dart';
 
 void main() {
   group('DateTimeUtilsTest', () {
-    test("getStartOfWeek", (){
+    test("getStartOfWeek", () {
       final date = DateTime(2026, 2, 19); // Четверг 19 февраля 2026 года
       // ignore: no_leading_underscores_for_local_identifiers
-      final _startOfWeek = DateTime(2026, 2, 16); // Понедельник 16 февраля 2026 года
+      final _startOfWeek =
+          DateTime(2026, 2, 16); // Понедельник 16 февраля 2026 года
       final startOfWeek = DateTimeUtils.getStartOfWeek(date);
       expect(startOfWeek.day, _startOfWeek.day);
       expect(startOfWeek.month, _startOfWeek.month);
@@ -15,17 +16,19 @@ void main() {
     test('getEndOfWeek', () {
       final date = DateTime(2026, 2, 19); // Четверг 19 февраля 2026 года
       // ignore: no_leading_underscores_for_local_identifiers
-      final _endOfWeek = DateTime(2026, 2, 22); // Воскресенье 22 февраля 2026 года
+      final _endOfWeek =
+          DateTime(2026, 2, 22); // Воскресенье 22 февраля 2026 года
       final endOfWeek = DateTimeUtils.getEndOfWeek(date);
       expect(endOfWeek.day, _endOfWeek.day);
       expect(endOfWeek.month, _endOfWeek.month);
       expect(endOfWeek.year, _endOfWeek.year);
     });
-    
+
     test('formatDate', () {
       final date = DateTime(2026, 2, 19); // Четверг 19 февраля 2026 года
       final formattedDate = DateTimeUtils.formatDate(date);
-      final formattedDateWithoutWeekDay = DateTimeUtils.formatDate(date, showWeekday: false);
+      final formattedDateWithoutWeekDay =
+          DateTimeUtils.formatDate(date, showWeekday: false);
       expect(formattedDate, 'Четверг, 19.02.2026');
       expect(formattedDateWithoutWeekDay, '19.02.2026');
     });
@@ -49,16 +52,18 @@ void main() {
       expect(parsedDate?.year, 2026);
     });
 
-    test("isToday", (){
+    test("isToday", () {
       final date = DateTime.now();
       expect(DateTimeUtils.isToday(date), true);
-      expect(DateTimeUtils.isToday(date.subtract(const Duration(days: 1))), false);
+      expect(
+          DateTimeUtils.isToday(date.subtract(const Duration(days: 1))), false);
       expect(DateTimeUtils.isToday(date.add(const Duration(days: 1))), false);
     });
 
     test('IsCurrentDate', () {
       final date = DateTime.now();
-      String dateString = '${date.day.toString().padLeft(2, '0')}.${date.month.toString().padLeft(2, '0')}.${date.year}';
+      String dateString =
+          '${date.day.toString().padLeft(2, '0')}.${date.month.toString().padLeft(2, '0')}.${date.year}';
       expect(DateTimeUtils.isCurrentDate(date, dateString), true);
       expect(DateTimeUtils.isCurrentDate(date, '01.01.1999'), false);
     });
